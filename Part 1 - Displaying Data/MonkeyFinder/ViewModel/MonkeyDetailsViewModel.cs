12d19089a5246e403c407deb434dcb6df2409dc6
joonasoispuu <oispuu.joonas@gmail.com>
2023-06-02 20:01:50 +0300

    Added a button into DetailsPage so the user can find the monkeys location on the map

--- Part 1 - Displaying Data/MonkeyFinder/ViewModel/MonkeyDetailsViewModel.cs
+++ Part 1 - Displaying Data/MonkeyFinder/ViewModel/MonkeyDetailsViewModel.cs
@@ -1,12 +1,34 @@
-﻿namespace MonkeyFinder.ViewModel;
+﻿using Microsoft.Maui.ApplicationModel;
+
+namespace MonkeyFinder.ViewModel;
 
 [QueryProperty(nameof(Monkey), "Monkey")]
 public partial class MonkeyDetailsViewModel : BaseViewModel
 {
-    public MonkeyDetailsViewModel()
+    IMap map;
+    public MonkeyDetailsViewModel(IMap map)
     {
+        this.map = map;
     }
 
     [ObservableProperty]
     Monkey monkey;
+
+    [RelayCommand]
+    async Task OpenMap()
+    {
+        try
+        {
+            await map.OpenAsync(Monkey.Latitude, Monkey.Longitude, new MapLaunchOptions
+            {
+                Name = Monkey.Name,
+                NavigationMode = NavigationMode.None
+            });
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine($"Unable to launch maps: {ex.Message}");
+            await Shell.Current.DisplayAlert("Error, no Maps app!", ex.Message, "OK");
+        }
+    }
 }
